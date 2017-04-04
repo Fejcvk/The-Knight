9607125f84d38068c8fd7cfc301e99ef68a1fcbd
Fejcvk <tomaszfejcak@icloud.com>
2017-04-05 00:02:09 +0200

    Init commit

---
+++ The_Knight/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace The_Knight
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this._pnlBoard = new System.Windows.Forms.TableLayoutPanel();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripDropDownButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(818, 25);
+            this.toolStrip1.TabIndex = 2;
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem4,
+            this.settingsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(78, 22);
+            this.toolStripDropDownButton1.Text = "New Game";
+            this.toolStripDropDownButton1.DropDownItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.toolStripDropDownButton1_DropDownItemClicked);
+            // 
+            // toolStripMenuItem4
+            // 
+            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(152, 22);
+            this.toolStripMenuItem4.Tag = "14";
+            this.toolStripMenuItem4.Text = "New Game";
+            this.toolStripMenuItem4.Click += new System.EventHandler(this.toolStripMenuItem4_Click);
+            // 
+            // _pnlBoard
+            // 
+            this._pnlBoard.ColumnCount = 8;
+            this._pnlBoard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this._pnlBoard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this._pnlBoard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this._pnlBoard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this._pnlBoard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this._pnlBoard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this._pnlBoard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this._pnlBoard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this._pnlBoard.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._pnlBoard.Location = new System.Drawing.Point(0, 25);
+            this._pnlBoard.Name = "_pnlBoard";
+            this._pnlBoard.RowCount = 8;
+            this._pnlBoard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this._pnlBoard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this._pnlBoard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this._pnlBoard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this._pnlBoard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this._pnlBoard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this._pnlBoard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this._pnlBoard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this._pnlBoard.Size = new System.Drawing.Size(818, 586);
+            this._pnlBoard.TabIndex = 1;
+            this._pnlBoard.CellPaint += new System.Windows.Forms.TableLayoutCellPaintEventHandler(this._pnlBoard_CellPaint);
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            this.settingsToolStripMenuItem.Click += new System.EventHandler(this.settingsToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(818, 611);
+            this.Controls.Add(this._pnlBoard);
+            this.Controls.Add(this.toolStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "The Knight";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
+        private System.Windows.Forms.TableLayoutPanel _pnlBoard;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ The_Knight/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace The_Knight
+{
+    public partial class Form1 : Form
+    {
+        private int[,] _board;
+        private Random _random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void StartNewGame(int boardSize)
+        {
+            _board = new int[boardSize, boardSize];
+            GenerateBoardView();
+            _pnlBoard.Enabled = true;
+        }
+
+
+        private void GenerateBoardView()
+        {
+            _pnlBoard.Controls.Clear();
+            _pnlBoard.ColumnStyles.Clear();
+            _pnlBoard.RowStyles.Clear();
+
+            _pnlBoard.ColumnCount = _board.GetLength(0);
+            _pnlBoard.RowCount = _board.GetLength(1);
+
+            for (int i = 0; i < _pnlBoard.ColumnCount; ++i)
+            {
+                _pnlBoard.RowStyles.Add(new RowStyle(SizeType.Percent, (float)100.0 / _board.GetLength(0)));
+                _pnlBoard.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, (float)100.0 / _board.GetLength(1)));
+            }
+        }
+
+        private void toolStripDropDownButton1_DropDownItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+          // StartNewGame(int.Parse(e.ClickedItem.Tag.ToString()));
+        }
+
+
+        private int setRandom()
+        {
+            int random = _random.Next(0, 2);
+            return random;
+        }
+
+        private void _pnlBoard_CellPaint(object sender, TableLayoutCellPaintEventArgs e)
+        {
+            if ((e.Column + e.Row) % 2 == 1)
+            {
+                int colorValue = setRandom();
+                if (colorValue == 0)
+                {
+                    e.Graphics.FillRectangle(Brushes.Maroon, e.CellBounds);
+                }
+                else
+                {
+                    e.Graphics.FillRectangle(Brushes.ForestGreen, e.CellBounds);
+                }
+            }
+            else
+            {
+                int colorValue = setRandom();
+                if (colorValue == 0)
+                {
+                    e.Graphics.FillRectangle(Brushes.Maroon, e.CellBounds);
+                }
+                else
+                {
+                    e.Graphics.FillRectangle(Brushes.ForestGreen, e.CellBounds);
+                }
+            }
+        }
+        protected Boolean CanClose(Boolean CanIt)
+        {
+            if (MessageBox.Show("Wanna close?", "Cancel game", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                // Yes, they want to close.
+                CanIt = true;
+            }
+            else
+            {
+                // No, they don't want to close.
+                CanIt = false;
+            }
+
+            return CanIt;
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            if (CanClose(false) == true)
+            {
+                this.Dispose(true);
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+
+        public void Settings()
+        {
+            var setupBox = new Form2();
+            DialogResult result = setupBox.ShowDialog(this);
+            if (result == DialogResult.Abort)
+                setupBox.Dispose();
+            else
+            {
+               if(setupBox.comboBox1.SelectedIndex == 0)
+                {
+                    StartNewGame(8);
+                }
+                if (setupBox.comboBox1.SelectedIndex == 1)
+                {
+                    StartNewGame(10);
+                }
+                if (setupBox.comboBox1.SelectedIndex == 2)
+                {
+                    StartNewGame(12);
+                }
+            }
+        }
+
+
+        private void settingsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Settings();
+        }
+
+        private void toolStripMenuItem4_Click(object sender, EventArgs e)
+        {
+            StartNewGame(8);
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == (Keys.Control | Keys.N))
+            {
+                StartNewGame(8);
+                return true;
+            }
+            if (keyData == (Keys.Control | Keys.M))
+            {
+                Settings();
+                return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+    }
+}
+
